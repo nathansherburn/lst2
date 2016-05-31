1b1c87d2e3e89dace9ea157f0bb7568499a9c163
Nathan Sherburn <nathansherburn@gmail.com>
2016-05-31 10:15:51 +1000

    Add authentication

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+let basicAuth = require('basic-auth');
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lst2');
+let List = mongoose.model('List', { list: String, content: String });
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === process.env.USER && user.pass === process.env.PASS) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lst2');
-
-let List = mongoose.model('List', { list: String, content: String });
+app.use(auth);
 
 app.get('/data', function (req, res) {
   List.find({}, function(err, lists) {
@@ -30,6 +49,5 @@ app.post('/data', function(req, res) {
       else res.send(list)
   });
 });
-
 
 app.listen(process.env.PORT || 8080);
